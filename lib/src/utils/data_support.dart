c1a5b3275c3772ce29d8960c8ff5566fbfc4b891
nguu <39035911+nguu@users.noreply.github.com>
2019-10-30 14:57:07 +0700

    Update project icons

--- lib/src/utils/data_support.dart
+++ lib/src/utils/data_support.dart
@@ -2,28 +2,95 @@ import 'package:flutter/material.dart';
 
 class DataSupport {
   static const Map<String, IconData> icons = {
-    'folder': Icons.folder,
-    'build': Icons.build,
+    // Action
+    'account_balance': Icons.account_balance,
+    'account_balance_wallet': Icons.account_balance_wallet,
+    'android': Icons.android,
+    'announcement': Icons.announcement,
+    'book': Icons.book,
     'bookmark': Icons.bookmark,
-    'folder_shared': Icons.folder_shared,
-    'work': Icons.work,
-    'shopping_cart': Icons.shopping_cart,
-    'info': Icons.info,
-    'favorite': Icons.favorite,
-    'cloud': Icons.cloud,
-    'toys': Icons.toys,
-    'photo': Icons.photo,
-    'fitness_center': Icons.fitness_center,
-    'home': Icons.home,
+    'build': Icons.build,
+    'bug_report': Icons.bug_report,
     'code': Icons.code,
+    'credit_card': Icons.credit_card,
+    'dashboard': Icons.dashboard,
+    'date_range': Icons.date_range,
+    'delete': Icons.delete,
+    'done': Icons.done,
+    'event': Icons.event,
+    'explore': Icons.explore,
+    'extension': Icons.extension,
+    'face': Icons.face,
+    'favorite': Icons.favorite,
+    'feedback': Icons.feedback,
+    'fingerprint': Icons.fingerprint,
+    'help': Icons.help,
+    'home': Icons.home,
+    'info': Icons.info,
+    'label': Icons.label,
+    'language': Icons.language,
+    'list': Icons.list,
+    'pets': Icons.pets,
+    'print': Icons.print,
+    'room': Icons.room,
+    'shopping_basket': Icons.shopping_basket,
+    'shopping_cart': Icons.shopping_cart,
+    'stars': Icons.stars,
+    'store': Icons.store,
+    'thumb_down': Icons.thumb_down,
+    'thumb_up': Icons.thumb_up,
+    'timeline': Icons.timeline,
+    'work': Icons.work,
+
+    // Communication
+    'business': Icons.business,
+    'email': Icons.email,
+    'phone': Icons.phone,
+
+    // Content
+    'add_box': Icons.add_box,
+    'archive': Icons.archive,
+    'flag': Icons.flag,
+    'report': Icons.report,
+
+    // File
+    'attachment': Icons.attachment,
+    'cloud': Icons.cloud,
+    'folder': Icons.folder,
+    'folder_shared': Icons.folder_shared,
+
+    // Hardware
+    'computer': Icons.computer,
+    'smartphone': Icons.smartphone,
+    'toys': Icons.toys,
+
+    // Image
+    'audiotrack': Icons.audiotrack,
+    'camera': Icons.camera,
+    'collections': Icons.collections,
+    'color_lens': Icons.color_lens,
+    'landscape': Icons.landscape,
+    'photo': Icons.photo,
+    'style': Icons.style,
+
+    // Places
+    'ac_unit': Icons.ac_unit,
+    'casino': Icons.casino,
+    'fitness_center': Icons.fitness_center,
+
+    // Social
+    'cake': Icons.cake,
+    'group': Icons.group,
+    'notifications': Icons.notifications,
     'person': Icons.person,
     'public': Icons.public,
-    'share': Icons.share
+    'school': Icons.school,
+    'share': Icons.share,
   };
 
   static const Map<String, Color> colors = {
-    'green': Colors.green,
     'blue': Colors.blue,
+    'green': Colors.green,
     'purple': Colors.purple,
     'red': Colors.red,
     'pink': Colors.pink,
